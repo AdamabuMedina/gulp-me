074b619ca462f7c57e23b501da37cca9080ff8a5
adamabumedina <adamabujamila@gmail.com>
2021-12-21 14:43:41 +0300

    js settings

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const path = require("./config/path.js")
 const clear = require("./task/clear.js")
 const html = require("./task/html.js")
 const scss = require("./task/scss.js")
+const js = require("./task/js.js")
 
 // Сервер
 const server = () => {
@@ -22,16 +23,18 @@ const server = () => {
 const wathcer = () => {
   watch(path.html.watch, html).on("all", browsersync.reload);
   watch(path.scss.watch, scss).on("all", browsersync.reload);
+  watch(path.js.watch, js).on("all", browsersync.reload);
 }
 
 // Задачи
 exports.html = html;
 exports.scss = scss;
+exports.js = js;
 
 
 // Сборка
 exports.dev = series(
   clear,
-  parallel(html, scss),
+  parallel(html, scss, js),
   parallel(wathcer, server)
 )
