4100ba68b4fd840c5592ac79dacaf638f713b548
adamabumedina <adamabujamila@gmail.com>
2021-12-21 12:30:36 +0300

    config and css install

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,27 @@
-const { src, dest, watch, series, parallel } = require("gulp");
+const { watch, series, parallel } = require("gulp");
 const browsersync = require("browser-sync").create();
-const del = require("del");
 
-// плагины
-const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
-const fileinclude = require("gulp-file-include");
-const htmlmin = require("gulp-htmlmin");
-const size = require("gulp-size");
+// Конфигурация
+const path = require("./config/path.js")
 
-
-// Обработка html
-const html = () => {
-  return src("./src/html/*.html")
-    .pipe(plumber({
-      errorHandler: notify.onError(error => ({
-        title: "HTML",
-        message: error.message
-      }))
-    }))
-    .pipe(fileinclude())
-    .pipe(size({ title: "До сжатия" }))
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(size({ title: "После сжатия" }))
-    .pipe(dest("./dist"))
-    .pipe(browsersync.stream())
-}
-
-// Удаление директории
-const clear = () => {
-  return del("./dist")
-}
 // Сервер
 const server = () => {
   browsersync.init({
     server: {
-      baseDir: "./dist"
+      baseDir: path.root
     }
   })
 }
 
-
 // Наблюдение
 const wathcer = () => {
-  watch("./src/html/**/*.html", html);
+  watch(path.html.watch, html).on("all", browsersync.reload);
 }
 
 
 // Задачи
-exports.html = html;
-exports.wathcer = wathcer;
-exports.clear = clear;
+const clear = require("./task/clear.js")
+const html = require("./task/html.js")
 
 
 // Сборка
